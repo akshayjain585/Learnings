a82b03c414b8072cd6b59a97876e4bc3993f3fba
akshayjain585 <akshayjain585@gmail.com>
2018-11-01 00:02:23 +0530

    Commiting all files at Hyd Airport

--- Learnings/src/arraysPrograms/SubArray.java
+++ Learnings/src/arraysPrograms/SubArray.java
@@ -1,8 +1,11 @@
 package arraysPrograms;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class SubArray {
 	
-	boolean isSubArray(int[] list1, int[] list2) {
+	static boolean isSubArray(int[] list1, int[] list2) {
 		for(int i=0; i<list2.length; i++) {
 			for(int j=0; j<list1.length; j++) {
 				if(list2[i]==list1[j]) {
@@ -11,6 +14,22 @@ public class SubArray {
 			}
 		}
 		return false;
+	}
+	
+	public static boolean isSub(int[] list1, int[] list2){
+		boolean status = false;
+		Set<Integer> set = new HashSet<>();
+		for(int key : list2){
+			set.add(key);
+		}
+		
+		for(int key : list1){
+			if(set.contains(key)){
+				status = true;
+			}
+		}
+		
+		return status;
 	}
 
 }
